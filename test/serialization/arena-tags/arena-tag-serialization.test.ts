52db730b8d18470a860bd367ba422a09d454efd6
Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>
2025-08-01 13:12:47 -0600

    Add serialization tests

---
+++ test/serialization/arena-tags/arena-tag-serialization.test.ts
@@ -0,0 +1,31 @@
+import { getArenaTag, SerializableArenaTag } from "#data/arena-tag";
+import { ArenaTagSide } from "#enums/arena-tag-side";
+import { ArenaTagType } from "#enums/arena-tag-type";
+import { MoveId } from "#enums/move-id";
+import { Z$ArenaTag } from "#system/schemas/arena/arena-tag";
+import { describe, expect, it } from "vitest";
+
+describe("Serialization - Arena Tags", () => {
+  // Serialize and deserialize each arena tag.
+
+  // Collect the arena tags for each arena tag type that is considered serializable.
+  describe("Round trip serialization", () => {
+    for (const arenaTagType of Object.values(ArenaTagType)) {
+      // Just use arbitrary data for the required fields.
+      const tag = getArenaTag(arenaTagType, 2, MoveId.TACKLE, 1, ArenaTagSide.PLAYER);
+      if (!(tag instanceof SerializableArenaTag)) {
+        continue;
+      }
+
+      // Now, "serialize" the tag via toJSON and JSON.stringify.
+      it(`${arenaTagType}`, () => {
+        const deserialized = JSON.parse(JSON.stringify(tag));
+        // Now, try loading the tag
+        const parsed = Z$ArenaTag.parse(deserialized);
+
+        // Now, expect the fields of each tag to be the same.
+        expect(parsed).toEqual(tag);
+      });
+    }
+  });
+});
---
+++ test/serialization/illusion-data/illusion-data-migration.test.ts
@@ -0,0 +1,16 @@
+import { V1_10_IllusionDataMigrator } from "#system/schema-migrators/summon-data";
+import { Z$IllusionData } from "#system/schemas/pokemon/illusion-data";
+import { describe, expect, it } from "vitest";
+import testData from "./v1.9-v1.10.json";
+
+describe("Deserialize - Illusion Data", () => {
+
+  describe("Migration from v1.9 to v1.10", () => {
+    it.each(testData)("$testName", ({ $input, $output }) => {
+      const migratedData = V1_10_IllusionDataMigrator($input);
+      const parsedData = Z$IllusionData.parse(migratedData);
+      expect(parsedData).toEqual($output);
+    });
+  });
+  
+});
